8dec5e7c7812dad5fd9c44e47492b1975674f316
Vinay Chandran <vinaychandranvs@gmail.com>
2016-05-06 20:06:28 +0530

    Stronger encryption. Encoded characters for repeating characters are different now.

--- malbolge.py
+++ malbolge.py
@@ -1,37 +1,46 @@
 import random
-keyBase = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+'
-keyBaseArray = list(keyBase)
-codeKeyArray = keyBaseArray
+
 
 def safeLock():
     shuffleKey=raw_input('Enter the shuffleKey :')
     #sets the shuffleKey value as the randomizing parameter
-    random.seed(shuffleKey)
-    random.shuffle(codeKeyArray)
     #Checks if the entered key is a safeLock key or not
     if (shuffleKey != '1996'):
         print "shuffleKey accepted! Running..."
-        callMenu()
+        callMenu(shuffleKey)
     else :
         #Yet to add: A set of safe messages will be printed as the result of decryption
         print "Initiating safeLock! Print random safe messages here! ;)"
 
-def decipher():
-    #Code to find all possible shuffleKeys if a known encrypted-decrypted combination of words is entered
-def encrypt():
+def encrypt(shuffleKey):
+    keyBase = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+'
+    keyBaseArray = list(keyBase)
+    codeKeyArray = keyBaseArray
     sentence = raw_input("What do you want to encrypt? : ")
     charArray = list(sentence)
+    print shuffleKey
     for count1,elem in enumerate(charArray):
+        shuffleKey=int(shuffleKey)
+        shuffleKey+=count1
+        random.seed(shuffleKey)
+        random.shuffle(codeKeyArray)
         for count2,elem2 in enumerate(keyBase):
             if elem == elem2:
                 charArray[count1]=codeKeyArray[count2]
     string1=''.join(charArray)
     return string1
 
-def decrypt():
+def decrypt(shuffleKey):
+    keyBase = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+'
+    keyBaseArray = list(keyBase)
+    codeKeyArray = keyBaseArray
     sentence = raw_input("What do you want to decrypt? : ")
     charArray = list(sentence)
     for count1,elem in enumerate(charArray):
+        shuffleKey=int(shuffleKey)
+        shuffleKey+=count1
+        random.seed(shuffleKey)
+        random.shuffle(codeKeyArray)
         for count2,elem2 in enumerate(codeKeyArray):
             if elem == elem2:
                 keyBaseArray = list(keyBase)
@@ -39,16 +48,16 @@ def decrypt():
     string1=''.join(charArray)
     return string1
 
-def callMenu():
+def callMenu(shuffleKey):
     choice = 0
     while (choice!='3'):
         choice = raw_input("\n1.Encrypt \n2.Decrpyt \n3.Exit \nEnter your choice : ")
         if choice=="1":
-            answer=encrypt()
+            answer=encrypt(shuffleKey)
             print 'Encrypted sentence is : ' + '\033[1m\033[31m' + answer + '\033[0m'
 
         elif choice=="2":
-            answer=decrypt()
+            answer=decrypt(shuffleKey)
             print 'Decrypted sentence is : ' + '\033[1m\033[32m' + answer + '\033[0m'
         elif choice=="3":
             exit()
